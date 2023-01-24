071687ef4a97b0a573d193e7bb878e278b62a9d4
Marco Antonio <caldeiran391@gmail.com>
2023-01-23 22:26:18 -0300

    adicionando informações

---
+++ .history/listas_20230123183851.rb
@@ -0,0 +1,6 @@
+
+#Listas, Arrays ou vetores
+
+lista = [1, 2, 3, 9, "meu nome"]
+
+puts lista
---
+++ .history/listas_20230123183927.rb
@@ -0,0 +1,8 @@
+
+#Listas, Arrays ou vetores
+
+lista = [1, 2, 3, 9, "meu nome"]
+
+lista.push["Marco", "João"]
+
+puts lista
